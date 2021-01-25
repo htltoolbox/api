5a947e9a004c18badf2bcce2bbd90778697ad136
Philipp Lackinger <40075339+Phillackinger@users.noreply.github.com>
2021-01-25 14:30:19 +0100

    🔧 created dummy_env.py

---
+++ dummy_env.py
@@ -0,0 +1,22 @@
+# DB
+
+db_host = ""
+db_username = ""
+db_password = ""
+db_database = ""
+
+# GenericAPI Key
+
+key = ""
+
+# OAuth2
+
+SECRET_KEY = ""
+
+# Mail
+
+mail_host = ""
+mail_sender = ""
+mail_username = ""
+mail_password = ""
+mail_port = 465
