f3f81e82bf09ee1103b29782444c6ec035a20a13
Philipp Lackinger <Phillackinger@gmx.at>
2021-03-09 22:44:51 +0100

    :building_construction: Added APP -> API_KEY Query
    In this commit I added the possibility for the API to Query the APP Object with an API_KEY

--- functions/app.py
+++ functions/app.py
@@ -7,7 +7,7 @@ ds = datasource()
 ds.connect()
 
 
-def getApp(ID: Optional[int] = None, NAME: Optional[str] = None):
+def getApp(ID: Optional[int] = None, NAME: Optional[str] = None, API_KEY: Optional[str] = None):
     global SQL, PARAM
     if ID is not None:
         SQL = "SELECT * FROM toolbox.APPS WHERE ID = %s"
@@ -15,6 +15,9 @@ def getApp(ID: Optional[int] = None, NAME: Optional[str] = None):
     elif NAME is not None:
         SQL = "SELECT * FROM toolbox.APPS WHERE NAME = %s"
         PARAM = (NAME,)
+    elif API_KEY is not None:
+        SQL = "SELECT * FROM toolbox.APPS WHERE API_KEY = %s"
+        PARAM = (API_KEY,)
     ds.execute(SQL, PARAM)
     data = ds.fetch_dict()
     return App(**data)
